b7d8e7b2d3b984de24f7a651367d989ca09727a7
Sam <samuelgkeller@gmail.com>
2025-10-16 21:31:01 -0400

    chore: Add basic folder structure and main files

---
+++ tst/HotelDataAutomationTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HotelDataAutomationTest {
+
+    @Test
+    public void testHotelDataAutomation() {
+        HotelDataAutomation hotelDataAutomation = new HotelDataAutomation();
+    }
+
+}
